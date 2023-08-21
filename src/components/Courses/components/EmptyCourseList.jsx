854ad7cfb591c7abe55dc006782410bc0fd8812c
lukasz_plawiak <lukasz_plawiak@epam.com>
2023-08-21 13:36:32 +0200

    react-course modeule2_task1 v5

--- src/components/Courses/components/EmptyCourseList.jsx
+++ src/components/Courses/components/EmptyCourseList.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '../../../common/Button/Button';
 
 function EmptyCourseList({ isAdmin }) {
-	const navigate = useNavigate();
-
 	return (
 		<div className='empty-course-list'>
 			<h2>Course List is Empty</h2>
 			<p>Please use "Add New Course" button to add your first course</p>
-			{isAdmin ? (
-				<Button
-					label='Add New Course'
-					onClick={() => navigate('/courses/add')}
-				/>
-			) : (
+			{isAdmin && (
 				<p>
 					You don't have permissions to create a course. Please log in as ADMIN
 				</p>
