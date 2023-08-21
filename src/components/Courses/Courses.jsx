7d524eb2d8593dd08eb51d71dee4290beeb02fd9
Lukasz Plawiak (Contractor) <lukasz_plawiak@epam.com>
2023-08-21 12:00:51 +0000

    Modeule2 task1

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,40 +1,56 @@
 import { ADD_NEW_COURSE_LABEL } from '../../common/Constants/Constants';
 
 import React, { useState, useMemo } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import CourseCard from './components/CourseCard/CourseCard';
 import SearchBar from './components/SearchBar/SearchBar';
 import Button from '../../common/Button/Button';
+import EmptyCourseList from './components/EmptyCourseList';
 
 import './Courses.css';
 
-function Courses(props) {
+function Courses({ courses, authors, isAdmin }) {
 	const [query, setQuery] = useState('');
+	const navigate = useNavigate();
 
 	const handleSearch = (inputQuery) => {
 		setQuery(inputQuery);
 	};
 
 	const filteredCourses = useMemo(() => {
-		return props.courses.filter(
+		return courses.filter(
 			(course) =>
 				course.title.toLowerCase().includes(query.toLowerCase()) ||
 				course.id.toString().includes(query)
 		);
-	}, [props.courses, query]);
+	}, [courses, query]);
+
+	const handleAddNewCourse = () => {
+		navigate('/courses/add');
+	};
+
+	const handleCourseSelect = (course) => {
+		navigate(`/courses/${course.id}`);
+	};
 
 	return (
 		<div className='Courses'>
 			<SearchBar onSearch={handleSearch} />
-			{filteredCourses.map((course) => (
-				<CourseCard
-					key={course.id}
-					course={course}
-					authors={props.authors}
-					onCourseSelect={props.onCourseSelect}
-				/>
-			))}
-			<Button label={ADD_NEW_COURSE_LABEL} />
+			{filteredCourses.length > 0 ? (
+				filteredCourses.map((course) => (
+					<CourseCard
+						key={course.id}
+						course={course}
+						authors={authors}
+						onCourseSelect={handleCourseSelect}
+					/>
+				))
+			) : (
+				<EmptyCourseList isAdmin={isAdmin} />
+			)}
+			{isAdmin && (
+				<Button label={ADD_NEW_COURSE_LABEL} onClick={handleAddNewCourse} />
+			)}
 		</div>
 	);
 }
