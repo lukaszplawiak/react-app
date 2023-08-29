f8d312a7fdbd23b24492c03df60ac9f6fc88b8c7
Łukasz Pławiak <lukaszplawiakzero1@gmail.com>
2023-08-29 08:33:21 +0200

    Module3 task1 (#3)
     module3_task1 - redux

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,20 +1,28 @@
-import { ADD_NEW_COURSE_LABEL } from '../../common/Constants/Constants';
-
 import React, { useState, useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import CourseCard from './components/CourseCard/CourseCard';
 import SearchBar from './components/SearchBar/SearchBar';
 import Button from '../../common/Button/Button';
 import EmptyCourseList from './components/EmptyCourseList';
-
+import { ADD_NEW_COURSE_LABEL } from '../../common/Constants/Constants';
+import { deleteCourse } from '../../store/courses/actions';
 import './Courses.css';
 
-function Courses({ courses, authors, isAdmin }) {
-	const [query, setQuery] = useState('');
+function Courses() {
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
+	const courses = useSelector((state) => state.courses.courses);
+	const authors = useSelector((state) => state.authors.authors);
+	const user = useSelector((state) => state.user);
+	const [query, setQuery] = useState('');
 
 	const handleSearch = (inputQuery) => {
 		setQuery(inputQuery);
+	};
+
+	const handleDeleteCourse = (courseId) => {
+		dispatch(deleteCourse(courseId));
 	};
 
 	const filteredCourses = useMemo(() => {
@@ -43,12 +51,13 @@ function Courses({ courses, authors, isAdmin }) {
 						course={course}
 						authors={authors}
 						onCourseSelect={handleCourseSelect}
+						onDelete={handleDeleteCourse}
 					/>
 				))
 			) : (
-				<EmptyCourseList isAdmin={isAdmin} />
+				<EmptyCourseList isAdmin={user.isAdmin} />
 			)}
-			{isAdmin && (
+			{user.isAdmin && (
 				<Button label={ADD_NEW_COURSE_LABEL} onClick={handleAddNewCourse} />
 			)}
 		</div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import App from './App';
-
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = document.getElementById('root');
 const appRoot = createRoot(root);
 
 appRoot.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 );
