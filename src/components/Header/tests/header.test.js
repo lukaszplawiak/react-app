8ccff1fc4006679d9af6493bc609d324425333d3
lukasz_plawiak <lukasz_plawiak@epam.com>
2023-08-30 15:28:07 +0200

    module5_task1 - tests

---
+++ src/components/Header/tests/header.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import Header from '../Header';
+import { MemoryRouter as Router } from 'react-router-dom';
+import rootReducer from '../../../store/rootReducer';
+
+const initialState = {
+	user: {
+		isAuth: true,
+		name: 'username',
+	},
+	courses: [],
+	authors: [],
+};
+
+const store = configureStore({
+	reducer: rootReducer,
+	preloadedState: initialState,
+});
+
+describe('Header Component', () => {
+	beforeEach(() => {
+		render(
+			<Provider store={store}>
+				<Router>
+					<Header />
+				</Router>
+			</Provider>
+		);
+	});
+
+	it('should contain a logo', () => {
+		expect(screen.getByAltText(/logo/i)).toBeInTheDocument();
+	});
+
+	it('should display the username', () => {
+		expect(screen.getByText('Hello, username')).toBeInTheDocument();
+	});
+});
